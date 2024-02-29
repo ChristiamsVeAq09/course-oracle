2dabde972f6051a4aeb61c2182ed9105e3152d3e
ChristiamsVeAq09 <161747290+ChristiamsVeAq09@users.noreply.github.com>
2024-02-29 13:13:06 -0500

    Add files via upload

---
+++ Ejercicio02.sql
@@ -0,0 +1,5 @@
+--Listar a todos los customers que sean de los paises: France, Germany, Sweden. Guardar el archivo como "ejercicio02.sql" y subes a tu repositorio.
+SELECT * FROM Customers
+Where Country = 'France' 
+OR Country = 'Germany' 
+Or Country = 'Sweden'
