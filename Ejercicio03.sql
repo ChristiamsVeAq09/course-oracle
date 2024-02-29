0f56be7bcdef4826fafcb5ffa18f6875a922a9f8
ChristiamsVeAq09 <161747290+ChristiamsVeAq09@users.noreply.github.com>
2024-02-29 13:21:26 -0500

    Add files via upload

---
+++ Ejercicio03.sql
@@ -0,0 +1,3 @@
+--Listar todos los orders que el campo ShipRegion no sean NULL.Guardar el archivo como "ejercicio03.sql" y los subes a tu repositorio
+SELECT * FROM Customers
+Where Region NOT LIKE 'NULL'
