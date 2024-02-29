f4c7267e08dabb0579bad25f564ff44375ec655d
ChristiamsVeAq09 <161747290+ChristiamsVeAq09@users.noreply.github.com>
2024-02-29 12:47:17 -0500

    Add files via upload

---
+++ Ejercicio01.sql
@@ -0,0 +1,3 @@
+--Listar todos los customers que inicien su id con la letra 'A'. Guardas el archivo como "ejercicio01.sql" y lo subes a tu repositorio
+
+SELECT * FROM Customers WHERE CustomerID like 'A%'
